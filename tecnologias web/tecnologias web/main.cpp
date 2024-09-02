a2552ab386410fd3f0419cc6a0031b602c9ed576
ByAlexShark <alexanderzafiro01@gmail.com>
2024-09-02 17:58:01 -0400

    primer commit

---
+++ tecnologias web/tecnologias web/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tecnologias web
+//
+//  Created by Alexander Rojas on 2/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
